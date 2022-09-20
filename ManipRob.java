dff084cd4c554410449b398ba1cbe3ac05dd9243
kappablanka <theo.piacentini@gmail.com>
2022-09-20 09:20:11 +0200

    Debut

---
+++ ManipRob.java
@@ -0,0 +1,9 @@
+public class ManipRob {
+    public static void main (String[] args){
+        toto = new Robot("Toto", 10, 20, 2);
+        titi = new Robot("Titi");
+        toto.deplacer();
+
+
+    }
+}
